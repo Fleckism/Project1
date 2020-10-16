90a10f4bcd4dcd369f28201f04d3a268676ccca0
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-16 10:01:40 -0400

    Why is this so hard to do... Should be simple

--- git_test.cpp
+++ git_test.cpp
@@ -4,15 +4,7 @@ using namespace std;
 
 int main()
 {
-	int x = 0;
-	int y = 0;
-	int current_position[2] = { 0,0 };
-	char movement = 'Z';
-
-	while (movement != 'Q')
-	{
-	cout << " Current Position = (" << x << ',' << y << ')' << endl;
-	cout << " Move (N)orth, (E)ast, (W)est, (S)outh, (Q)uit? " << endl;
+	FUCK YOU JAMIE FLECK AND YOUR GOALS, YOU CAN GO FUCK YOURSELF... GIT
 
 
 	}
