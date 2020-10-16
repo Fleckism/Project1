2a8734cdfd817689403291470b8e866290f546bf
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-16 12:36:22 -0400

    2.7.2 Array

--- git_test.cpp
+++ git_test.cpp
@@ -4,51 +4,64 @@ using namespace std;
 
 int main()
 {
-	int x = 0;  // east to west movement
-	int y = 0;	// north to south movement
-	//int current_position[2] = { 0,0 }; // stores the current position
-	char movement = 'z';
+	int x = 0; //East to West
+	int y = 0; // North to South
 
-	while (movement != 'q')
+	int current_position[2] = { x,y };
+	char direction_travel = 'Z';
+
+	while (direction_travel != 'Q')
 	{
-	cout << " current position = (" << x << ',' << y << ')' << endl;
-	cout << " move (n)orth, (e)ast, (w)est, (s)outh, (q)uit? " << endl;
-	cin >> movement;
+		cout << "Current Position = (" << current_position[x] << ',' << current_position[y] << ')' << endl;
+		cout << "Move (N)orth, (E)ast, (W)est, (S)outh, (Q)uit?" << endl;
 
-	switch (movement)
-	{
-	case 'n':
-		++y;//current_position[1]++;
-		break;
+		cin >> direction_travel;
 
-	case 's':
-		--y;
-		break;
+		switch (direction_travel)
+		{
+		case 'N':
+			current_position[y] = ++y;
+			break;
 
-	case 'e':
-		--x;
-		break;
+		case 'n':
+			current_position[y] = ++y;
+			break;
 
-	case 'w':
-		++x;
-		break;
+		case 'S':
+			current_position[y] = --y;
+			break;
 
-	case 'q':
-		cout <<"exiting..." << endl;
-		break;
-		
-	default:
-		cout << " invalid entry " << endl;
+		case 's':
+			current_position[y] = --y;
+			break;
 
+		case 'E':
+			current_position[x] = --y;
+			break;
+
+		case 'e':
+			current_position[x] = --y;
+			break;
+
+		case 'W':
+			current_position[x] = ++y;
+			break;
+
+		case 'w':
+			current_position[x] = ++y;
+			break;
+
+
+		case 'Q':
+			cout << "Exiting Program..." << endl;
+			break;
+
+		case 'q':
+			cout << "Exiting Program..." << endl;
+			break;
+
+		default:
+			cout << "Invalid entry." << endl;
 		}
-
-
-
-
-
-
-
-
 	}
-	return 0;
 }
