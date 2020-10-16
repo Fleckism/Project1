c37f94e5fceb1c4e48e2047a929d0c4ab42bf05c
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-16 09:38:02 -0400

    2.7.2 Navigator

--- git_test.cpp
+++ git_test.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello, Git!" << endl;
+	int x = 0;
+	int y = 0;
+	int current_position[2] = { 0,0 };
+	char movement = 'Z';
+
+	while (movement != 'Q')
+	{
+	cout << " Current Position = (" << x << ',' << y << ')' << endl;
+	cout << " Move (N)orth, (E)ast, (W)est, (S)outh, (Q)uit? " << endl;
+
+
+	}
 }
