6c343e9a248d0a76c201987579d5eeef668ba242
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-16 09:38:57 -0400

    Deleted Everything

--- git_test.cpp
+++ git_test.cpp
@@ -4,16 +4,5 @@ using namespace std;
 
 int main()
 {
-	int x = 0;
-	int y = 0;
-	int current_position[2] = { 0,0 };
-	char movement = 'Z';
-
-	while (movement != 'Q')
-	{
-	cout << " Current Position = (" << x << ',' << y << ')' << endl;
-	cout << " Move (N)orth, (E)ast, (W)est, (S)outh, (Q)uit? " << endl;
-
-
-	}
+	// I deleted everything Muahhhhh
 }
