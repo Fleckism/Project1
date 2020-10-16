65de1c14e4eb1a81f9f7d2097489ab52020999b1
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-16 10:46:08 -0400

    Completed 2.7.2 saved file in vim incase Git shits the bed.

--- git_test.cpp
+++ git_test.cpp
@@ -4,16 +4,51 @@ using namespace std;
 
 int main()
 {
-	int x = 0;
-	int y = 0;
-	int current_position[2] = { 0,0 };
+	int x = 0;  // East to West movement
+	int y = 0;	// North to South movement
+	//int current_position[2] = { 0,0 }; // Stores the current position
 	char movement = 'Z';
 
 	while (movement != 'Q')
 	{
 	cout << " Current Position = (" << x << ',' << y << ')' << endl;
 	cout << " Move (N)orth, (E)ast, (W)est, (S)outh, (Q)uit? " << endl;
+	cin >> movement;
+
+	switch (movement)
+	{
+	case 'N':
+		++y;//current_position[1]++;
+		break;
+
+	case 'S':
+		--y;
+		break;
+
+	case 'E':
+		--x;
+		break;
+
+	case 'W':
+		++x;
+		break;
+
+	case 'Q':
+		cout <<"Exiting..." << endl;
+		break;
+		
+	default:
+		cout << " Invalid entry " << endl;
+
+		}
+
+
+
+
+
+
 
 
 	}
+	return 0;
 }
