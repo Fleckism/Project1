d690ec762e8176e2f76b612a9ff5a845427d2147
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-19 21:13:34 -0400

    002, 2.7.4 factorial

--- git_test.cpp
+++ git_test.cpp
@@ -9,14 +9,18 @@ using namespace std;
 int main()
 {
     int fact_in = 0;
-    int factor = 0;
-    int product = 0;
+    int factor_array[25];
+    int product =0;
     cout << " Enter a positive integer to compute the factorial of :";
     cin >> fact_in;
 
-    for (int i=0; fact_in <= i; ++i 
+    for (int i=0; fact_in <= i; ++i )
         {
-            product = fact_in 
+        cout << "Evaluate " << fact_in << "!." << endl;
+        cout << fact_in << "!" << " + " << endl;
+
+        cin >> factor_array[i]
+        factor_array [i]= fact_in 
         }
 
 
