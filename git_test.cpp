bef0195508e7bce61ef6bff6fb3d9546d05f27b3
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-22 20:57:07 -0400

    274 Complete

--- git_test.cpp
+++ git_test.cpp
@@ -10,14 +10,14 @@ using namespace std;
 int main()
 {
     int row = 2, column = 3, sum[2][3], i, j; // i and j are initialized here instead of every loop
-    int a[2][3] = { {0,1,2} , {3,4,5} };
-    int b[2][3] = { {6,7,8} , {9,10,11} };
+    int array_A[2][3] = { {0,1,2} , {3,4,5} };
+    int array_B[2][3] = { {6,7,8} , {9,10,11} };
 
     cout << "A = " << endl;
 
     for (i = 0; i < row; ++i) {
         for (j = 0; j < column; ++j)
-            cout << a[i][j] << "  "; // messed with [i][j]
+            cout << array_A[i][j] << "  "; // messed with [i][j]
         cout << endl;
     }
     cout << endl;
@@ -25,7 +25,7 @@ int main()
     cout << "B = " << endl;
     for (i = 0; i < row; ++i) {
         for (j = 0; j < column; ++j)
-            cout << b[i][j] << "  ";
+            cout << array_B[i][j] << "  ";
         cout << endl;
     }
     cout << endl;
@@ -33,7 +33,7 @@ int main()
 
     for (i = 0; i < row; ++i)
         for (j = 0; j < column; ++j)
-            sum[i][j] = a[i][j] + b[i][j];
+            sum[i][j] = array_A[i][j] + array_B[i][j];
 
     cout << "A + B = " << endl;
     for (i = 0; i < row; ++i) {
@@ -44,3 +44,4 @@ int main()
 
 
     return 0;
+}
