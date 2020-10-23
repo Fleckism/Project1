75da3ba845cba99a0c2dfdfdebe5c5296d1e87d8
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-22 21:46:59 -0400

    276 complete ASCII No idea where 275 is...

--- git_test.cpp
+++ git_test.cpp
@@ -1,6 +1,6 @@
-/* 2.7.5 Matrix Addition pg 64
-The sum of two matrices (two dimensional array?)  matrices A & B will be added and stored in C
-
+/* 2.7.6 ASCII pg 65
+Write a program that outputs every character between [33,255]
+  ascii #: ascii symbol
 */
 
 #include <iostream>
@@ -9,38 +9,19 @@ using namespace std;
 
 int main()
 {
-    int row = 2, column = 3, sum[2][3], i, j; // i and j are initialized here instead of every loop
-    int array_A[2][3] = { {0,1,2} , {3,4,5} };
-    int array_B[2][3] = { {6,7,8} , {9,10,11} };
-
-    cout << "A = " << endl;
-
-    for (i = 0; i < row; ++i) {
-        for (j = 0; j < column; ++j)
-            cout << array_A[i][j] << "  "; // messed with [i][j]
-        cout << endl;
-    }
-    cout << endl;
-
-    cout << "B = " << endl;
-    for (i = 0; i < row; ++i) {
-        for (j = 0; j < column; ++j)
-            cout << array_B[i][j] << "  ";
-        cout << endl;
-    }
-    cout << endl;
+   int i,j;
 
 
-    for (i = 0; i < row; ++i)
-        for (j = 0; j < column; ++j)
-            sum[i][j] = array_A[i][j] + array_B[i][j];
+   for (i = 33; i < 255; i++)
+   {
+           cout << i << ':' << (char)i << ' ';
+          // if (i % 105) no idea how to break the cout! bedtime
 
-    cout << "A + B = " << endl;
-    for (i = 0; i < row; ++i) {
-        for (j = 0; j < column; ++j)
-            cout << sum[i][j] << " ";
-        cout << endl;
-    }
+            //   cout << endl;
+       
+
+   }
+
 
 
     return 0;
