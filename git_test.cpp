b3fc183161955b6c3433e95877b1e8eda120d3e9
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-26 09:33:47 -0400

    275 Matrix Addition

--- git_test.cpp
+++ git_test.cpp
@@ -1,48 +1,46 @@
-/* 2.7.8 Selection Sort pg 66
+/* 275 Matrix addition
 
 */
 
-#include < iostream >
+#include <iostream>
+
 using namespace std;
 
-void selectionSort(int arr[]) {
-    for (int i = 0; i < 9; i++) {
-        int min = i;
+int main()
+{
+    int row = 2, column = 3, sum[2][3], i, j; // i and j are initialized here instead of every loop
+    int a[2][3] = { {0,1,2} , {3,4,5} };
+    int b[2][3] = { {6,7,8} , {9,10,11} };
 
-        for (int j = i + 1; j < 10; j++) {
-            if (arr[j] < arr[min]) {
-                min = j;
-            }
-        }
-        if (min != i) {
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-        }
-    }
-}
+    cout << "A = " << endl;
 
-int main() {
-
-    int my_Array[10];
-    cout << "Enter 10 integers in random order: " << endl;
-    //  Control loop for filling in 
-    for (int i = 0; i < 10; i++)
-    {
-        cin >> my_Array[i];
-    }
-
-    cout << "UNSORTED ARRAY: " << endl;
-    for (int i = 0; i < 10; i++) {
-        cout << my_Array[i] << "  ";
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < column; ++j)
+            cout << a[i][j] << " A ";
+        cout << endl;
     }
     cout << endl;
 
-    selectionSort(my_Array); // sorting actually happening
-
-    cout << "SORTED ARRAY: " << endl;
-    for (int i = 0; i < 10; i++) {
-        cout << my_Array[i] << "  ";
+    cout << "B = " << endl;
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < column; ++j)
+            cout << b[i][j] << " B ";
+        cout << endl;
     }
+    cout << endl;
+
+
+    for (i = 0; i < row; ++i)
+        for (j = 0; j < column; ++j)
+            sum[i][j] = a[i][j] + b[i][j];
+
+    cout << "A + B = " << endl;
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < column; ++j)
+            cout << sum[i][j] << " ";
+        cout << endl;
+    }
+
+
     return 0;
 }
