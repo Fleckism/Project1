f2f75f2f3b33c52160eb70b3735593a0632b5d6b
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-10-26 09:29:53 -0400

    278 Selection Sort

--- git_test.cpp
+++ git_test.cpp
@@ -1,43 +1,48 @@
-/* 2.7.7 Linear Search pg 66
+/* 2.7.8 Selection Sort pg 66
 
 */
 
-#include <iostream>
-
+#include < iostream >
 using namespace std;
 
-int main()
-{
-    int my_array[] = { 7,3,32,2,55,34,6,13,29,22,11,9,1,5,42,39,8 };
-    int my_value = 0;
-    int current_position = 0;
+void selectionSort(int arr[]) {
+    for (int i = 0; i < 9; i++) {
+        int min = i;
 
-    cout << " List = ";
-
-    for (int i = 0; i < 17; i++)
-    {
-        cout << my_array[i] << ',';
-    }
-
-    cout << endl << " Enter an interger in the list to search for : " << endl;
-    cin >> my_value; // 55 should = 4
-    
-
-    for (int j = 0; j < 17; j++)
-    {
-        if (my_array[j] == my_value)
-        {
-            current_position = j;
-            break;
+        for (int j = i + 1; j < 10; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
         }
-        
+        if (min != i) {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
 
-            
+int main() {
+
+    int my_Array[10];
+    cout << "Enter 10 integers in random order: " << endl;
+    //  Control loop for filling in 
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> my_Array[i];
     }
 
-    cout << " Item found at index [" << current_position << "]" << endl;
-        
+    cout << "UNSORTED ARRAY: " << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << my_Array[i] << "  ";
+    }
+    cout << endl;
 
+    selectionSort(my_Array); // sorting actually happening
 
+    cout << "SORTED ARRAY: " << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << my_Array[i] << "  ";
+    }
     return 0;
 }
